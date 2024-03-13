8acac96a1d0344d45246f5f470b0da4423382c70
Kader Mayora <103690851+xDefyy@users.noreply.github.com>
2024-03-13 20:22:27 +0100

    acabar

--- Main/Pandemic/src/Descodificar.java
+++ Main/Pandemic/src/Descodificar.java
@@ -2,19 +2,39 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Descodificar {
 
 	public static void main(String[] args) {
+
+		String ciudad;
 		
-		String nombreFichero = "ciudades";
+		int[] coordenadas = new int[3];
+		
+		String nombreFichero = "ciudadestest.txt";
 		try {
 			FileReader fileReader = new FileReader(nombreFichero);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String linea = null;
+			String linea = "";
+			String aux[] = linea.split(";");
 			
-
+			do {
 				
-
+				linea = bufferedReader.readLine();
+				
+				if (linea != null) {
+					aux = linea.split(";");	
+				}	
+	
+			} while (linea != null);				
+			
+			ciudad = aux[0];
+			coordenadas[0] = Integer.valueOf(aux[1]);
+			coordenadas[1] = Integer.valueOf(aux[2]);
+			coordenadas[2] = Integer.valueOf(aux[3]);
+			
+			System.out.println(ciudad  + " - " + coordenadas[0] + " - " + coordenadas[1] + " - " + coordenadas[2]);
+	
 			bufferedReader.close();
 			fileReader.close();
 
