d06f668706ba6ea45dd75a7325e68c181708e309
yliqi2 <1871649909yang@gmail.com>
2024-03-13 20:44:28 +0100

    Descodificar terminado
    Con lectura de todo el archivo

--- Main/Pandemic/src/Descodificar.java
+++ Main/Pandemic/src/Descodificar.java
@@ -7,11 +7,7 @@ public class Descodificar {
 
 	public static void main(String[] args) {
 
-		String ciudad;
-		
-		int[] coordenadas = new int[3];
-		
-		String nombreFichero = "ciudadestest.txt";
+		String nombreFichero = "ciudades.txt";
 		try {
 			FileReader fileReader = new FileReader(nombreFichero);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -24,17 +20,42 @@ public class Descodificar {
 				
 				if (linea != null) {
 					aux = linea.split(";");	
+					String coordsParse[] = aux[2].split(",");
+					String ciudad;
+					int[] coordenadas = new int[3];
+					String ciudadExtras[] = aux[3].split(","); 
+					
+					//guarda las variables del split a uno exterior
+					ciudad = aux[0];
+					coordenadas[0] = Integer.valueOf(aux[1]);
+					coordenadas[1] = Integer.valueOf(coordsParse[0]);
+					coordenadas[2] = Integer.valueOf(coordsParse[1]);
+					
+					
+					System.out.print("Silco actúa en " + ciudad  + " con los numeros " 
+					+ coordenadas[0] + ", " + coordenadas[1] + " y " + coordenadas[2] 
+							+ ", y cuyas ciudades colindantes son ");
+					
+					
+					for (int i = 0; i < ciudadExtras.length; i++) {
+						if (i != ciudadExtras.length-1) {
+							System.out.print(ciudadExtras[i] + " - " );
+						} else {
+							System.out.print(ciudadExtras[i] + "." );
+						}
+						
+					}
+					System.out.println();
+					
+					
 				}	
 	
 			} while (linea != null);				
 			
-			ciudad = aux[0];
-			coordenadas[0] = Integer.valueOf(aux[1]);
-			coordenadas[1] = Integer.valueOf(aux[2]);
-			coordenadas[2] = Integer.valueOf(aux[3]);
 			
-			System.out.println(ciudad  + " - " + coordenadas[0] + " - " + coordenadas[1] + " - " + coordenadas[2]);
-	
+			
+			
+			
 			bufferedReader.close();
 			fileReader.close();
 
